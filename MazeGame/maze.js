ef2edeb295fc95103b8ebab55270dcce9d9b8ea1
ADagnachew <84985193+ADagnachew@users.noreply.github.com>
2021-08-04 18:10:14 -0500

    jsClass Added

---
+++ MazeGame/maze.js
@@ -0,0 +1,30 @@
+$(function () {
+    let s = false;
+    let count = 0;
+    $(".boundary").mouseover(function () {
+        if (s === true) {
+            count = count + 1;
+            $('.boundary').not(".example").addClass("youlose");
+        }
+    });
+    $("#end").mouseover(function () {
+        if (s === true && count === 0)
+            $("#status").text("You win:]")
+        else
+            $("#status").text("You lose!")
+    });
+
+    $("#start").click(function () {
+        s = true;
+        $(".boundary").removeClass("youlose");
+        $("#status").text("Game started");
+
+    });
+    $("#maze").mouseleave(function () {
+        if (s === true) {
+            $("#status").text("You lose!");
+            $('.boundary').not(".example").addClass("youlose");
+        }
+
+    })
+});
