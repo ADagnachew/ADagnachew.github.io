e7bbe53e52712c58cedf0f884a756812625c08f3
ADagnachew <84985193+ADagnachew@users.noreply.github.com>
2021-08-04 18:33:48 -0500

    maze game

--- MazeGame/maze.js
+++ MazeGame/maze.js
@@ -4,18 +4,18 @@ $(function () {
     $(".boundary").mouseover(function () {
         if (s) {
             count = count + 1;
-            $('.boundary').not(".example").addClass("youlose");
-        }
-        else{
+            $(".boundary").not(".example").addClass("youlose");
+        } else {
             count = 0;
             $(".boundary").removeClass("youlose");
         }
     });
     $("#end").mouseover(function () {
-        if (s && !count)
-            $("#status").text("You win:]")
-        else
-            $("#status").text("Sorry,You lose!")
+        if (s && !count) {
+            $("#status").text("You win:]");
+        } else {
+            $("#status").text("Sorry,You lose!");
+        }
         s = false;
     });
     $("#start").click(function () {
@@ -26,7 +26,7 @@ $(function () {
     $("#maze").mouseleave(function () {
         if (s) {
             $("#status").text("You lose!");
-            $('.boundary').not(".example").addClass("youlose");
+            $(".boundary").not(".example").addClass("youlose");
         }
-    })
+    });
 });
